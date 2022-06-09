167a5087058856cf1a392d70bbe6c007bdafc397
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-10 00:21:30 +0200

    fetchCountries.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-import _ from 'lodash';
+import { debounce } from 'lodash';
 
 import './css/styles.css';
+
+import { fetchCountries } from './fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
