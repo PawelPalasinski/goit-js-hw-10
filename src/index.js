d34276aa8c0ffd0cf2a122e2e53a3c5a1b840f39
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-10 00:36:55 +0200

    js folder create

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { debounce } from 'lodash';
 
 import './css/styles.css';
 
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 
 
 const DEBOUNCE_DELAY = 300;
