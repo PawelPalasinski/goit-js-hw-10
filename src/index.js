f6a3846bb5f14c6e9dd3ab73dae7785a23304978
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-11 14:13:56 +0200

    fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-import { debounce } from 'lodash';
+import debounce from 'lodash.debounce';
 
 import './css/styles.css';
 
 import { fetchCountries } from './js/fetchCountries';
 
-
 const DEBOUNCE_DELAY = 300;
+
+const searchBox = document.querySelector("#search-box");
+
+
+
+
 
 // Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,12 @@
 export function fetchCountries(name) {
-
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => console.log(`${error.name}: ${error.message}`));
 }
