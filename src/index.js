c381576796925748c4fe5cbfb3a95af65f8e4e83
PawelPalasinski <pawel.palasinski@gmail.com>
2022-06-12 14:11:32 +0200

    consts for html el

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ import { fetchCountries } from './js/fetchCountries';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info"); \
+
 
 
 
